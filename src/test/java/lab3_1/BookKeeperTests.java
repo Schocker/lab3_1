72ad4b64244fbe729fb90a695b848b323bfd8202
= <=>
2018-03-27 18:51:43 +0200

    implementacja setUp

--- src/test/java/lab3_1/BookKeeperTests.java
+++ src/test/java/lab3_1/BookKeeperTests.java
@@ -3,6 +3,8 @@ package lab3_1;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Date;
 
 import org.junit.Before;
@@ -13,33 +15,34 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
-import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.*;
 import pl.com.bottega.ecommerce.sales.domain.invoicing.*;
-import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.*;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 @RunWith(MockitoJUnitRunner.class) 
 
 public class BookKeeperTests {
 	
-	private BookKeeper keeper;	
 	private InvoiceFactory invoiceFactory;	
-	BookKeeper bookKeeper;
-	TaxPolicy mockedTaxPolicy;
-	InvoiceRequest invoiceRequest;
+	private BookKeeper bookKeeper;
+	private TaxPolicy mockedTaxPolicy;
+	private InvoiceRequest invoiceRequest;
+	private ProductData productData;
 		
 	@Before
 	public void setUp() throws Exception {
 		bookKeeper = new BookKeeper(new InvoiceFactory());
 		mockedTaxPolicy = mock(TaxPolicy.class);
 		invoiceRequest = new InvoiceRequest(new ClientData(Id.generate(), "John Doe"));
-		productData = new 
+		Money moneyEUR = new Money(125.0);
+		Date snapshotDate = new Date();
+		productData = new ProductData(Id.generate(), moneyEUR, "Kartofel", ProductType.FOOD, snapshotDate);
 	}
 
 	@Test
 	public void requestingOneInvoiceShouldReturnInvoiceWithOnePosition() {
-		keeper.issuance(invoiceRequest, mockedTaxPolicy);
+		bookKeeper.issuance(invoiceRequest, mockedTaxPolicy);
 	}
 	
 	@Test
