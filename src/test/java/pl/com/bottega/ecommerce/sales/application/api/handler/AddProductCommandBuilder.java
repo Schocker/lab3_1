132b24a4aa90efff36ca4f41e4d20050a44452bd
Schocker <adikorzeniowski@gmail.com>
2018-04-29 14:11:07 +0200

    builder Create AddProductCommandBuilder

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandBuilder.java
@@ -0,0 +1,29 @@
+package pl.com.bottega.ecommerce.sales.application.api.handler;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
+
+public class AddProductCommandBuilder {
+    private Id orderId;
+    private Id productId;
+    private int quantity;
+
+    public AddProductCommandBuilder() {
+    }
+
+    public void withOrderId(Id orderId) {
+        this.orderId = orderId;
+    }
+
+    public void withProductId(Id productId) {
+        this.productId = productId;
+    }
+
+    public void withQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public AddProductCommand build() {
+        return new AddProductCommand(orderId, productId, quantity);
+    }
+}
